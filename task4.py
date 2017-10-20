04750bb54252e1a9058cb5ba7f1eaae0ace1c09a
Di Chen <31934037+DiChenB00747156@users.noreply.github.com>
2017-10-20 15:22:10 -0300

    final task

--- task4.py
+++ task4.py
@@ -1,21 +1,45 @@
+
 import requests
 import json
 
 controller='devnetapi.cisco.com/sandbox/apic_em'
-url = "https://" + controller + "/api/v1/ticket"
-payload = {"username":"devnetuser","password":"Cisco123!"}
-header = {"content-type": "application/json"}
-response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
-r_json=response.json()
-print(r_json)
-ticket = r_json["response"]["serviceTicket"]
 
-# URL for Host REST API call to get list of exisitng hosts on the network.
-url = "https://" + controller + "/api/v1/host"
-header = {"content-type": "application/json", "X-Auth-Token":ticket}
-response = requests.get(url, headers=header, verify=False)
-print ("Hosts = ")
-print (json.dumps(response.json(), indent=4, separators=(',', ': ')))
-r_resp=response.json()
-print(r_resp["response"][0]["hostIp"])
+
+class Network: #get host
+    template_name = "networkdevices.html"
+
+    def getTicket(self):
+        url = "https://" + controller + "/api/v1/ticket"
+        payload = {"username": "devnetuser", "password": "Cisco123!"}
+        header = {"content-type": "application/json"}
+        response = requests.post(url, data=json.dumps(payload), headers=header, verify=False)
+        r_json = response.json()
+        ticket = r_json["response"]["serviceTicket"]
+        return ticket
+
+
+    def getNetworkcount(self,ticket):
+        url = "https://" + controller + "/api/v1/network-device"
+        header = {"content-type": "application/json", "X-Auth-Token": ticket}
+        response = requests.get(url, headers=header, verify=False)
+        r_json = response.json() #get all information of devices
+        print("The counts of network devices is :")
+        print(len(r_json['response']))#get the counts of devices
+
+    def gethost(self, ticket):
+        url = "https://" + controller + "/api/v1/host"
+        header = {"content-type": "application/json", "X-Auth-Token": ticket}
+        response = requests.get(url, headers=header, verify=False)
+        r_json = response.json()
+        
+        for item in r_json['response']:
+            print("the ip address of this host is %s " % item['hostIp'])
+            print("the mac address of this host is %s " % item['hostMac'])
+            
+
+# create a object
+a = Network()
+ticket = a.getTicket()
+a.gethost(ticket)
+a.getNetworkcount(ticket)
 
