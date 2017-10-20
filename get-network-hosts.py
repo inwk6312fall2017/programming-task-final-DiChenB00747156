718dc0756e30839a6c3cbd3d8b9ada7c80092ef8
Di Chen <31934037+DiChenB00747156@users.noreply.github.com>
2017-10-20 15:24:55 -0300

    modify this file to get addresses of hosts

--- get-network-hosts.py
+++ get-network-hosts.py
@@ -21,49 +21,38 @@
 # * SUPPLIERS HAVE BEEN INFORMED OF THE POSSIBILITY THEREOF.-->
 
 
-# import requests library
 import requests
-
-#import json library
 import json
 
-controller='sandboxapic.cisco.com'
-#controller='devnetapi.cisco.com/sandbox/apic_em'
+controller='devnetapi.cisco.com/sandbox/apic_em'
 
 
-# put the ip address or dns of your apic-em controller in this url
-url = "https://" + controller + "/api/v1/ticket"
+class Network: #get host
+    template_name = "networkdevices.html"
 
-#the username and password to access the APIC-EM Controller
-payload = {"username":"devnetuser","password":"Cisco123!"}
+    def getTicket(self):
+        url = "https://" + controller + "/api/v1/ticket"
+        payload = {"username": "devnetuser", "password": "Cisco123!"}
+        header = {"content-type": "application/json"}
+        response = requests.post(url, data=json.dumps(payload), headers=header, verify=False)
+        r_json = response.json()
+        ticket = r_json["response"]["serviceTicket"]
+        return ticket
 
-#Content type must be included in the header
-header = {"content-type": "application/json"}
 
-#Performs a POST on the specified url to get the service ticket
-response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
+    def gethost(self, ticket):
+        url = "https://" + controller + "/api/v1/host"
+        header = {"content-type": "application/json", "X-Auth-Token": ticket}
+        response = requests.get(url, headers=header, verify=False)
+        r_json = response.json()
+        
+        for item in r_json['response']:
+            print("the ip address of this host is %s " % item['hostIp'])
+            print("the mac address of this host is %s " % item['hostMac'])
+            
 
-#convert response to json format
-r_json=response.json()
+# create a object
+a = Network()
+ticket = a.getTicket()
+a.gethost(ticket)
 
-print(r_json)
-#parse the json to get the service ticket
-ticket = r_json["response"]["serviceTicket"]
-
-# URL for Host REST API call to get list of exisitng hosts on the network.
-url = "https://" + controller + "/api/v1/host?limit=1&offset=1"
-
-#Content type must be included in the header as well as the ticket
-header = {"content-type": "application/json", "X-Auth-Token":ticket}
-
-# this statement performs a GET on the specified host url
-response = requests.get(url, headers=header, verify=False)
-
-# json.dumps serializes the json into a string and allows us to
-# print the response in a 'pretty' format with indentation etc.
-print ("Hosts = ")
-print (json.dumps(response.json(), indent=4, separators=(',', ': ')))
-
-r_resp=response.json()
-
-print(r_resp["response"][0]["hostIp"])
